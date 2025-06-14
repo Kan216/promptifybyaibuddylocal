09da565bff5e00b4923542d3164fdbd01ce90feb
Kan216 <127004433+Kan216@users.noreply.github.com>
2025-06-14 11:48:04 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,18 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7890617506854884"
      crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7890617506854884"
+     crossorigin="anonymous"></script>
+<!-- Test2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7890617506854884"
+     data-ad-slot="7192533618"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
